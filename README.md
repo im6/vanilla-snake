d65a79193f0453c5db365363ba2fca1c2b98c113
im6 <ee2win@gmail.com>
2018-10-17 19:06:12 +0800

    initial commit and set up development environment

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# vanilla_snake
+# Vanilla Snake Game
+
+=========================
+
+A javascript snake game with ES6 and nothing else.
+
+## cli instructions
+
+```
+npm install  // install all dependency for development
+npm start  //run local development on hot server
+npm run build  //Make a production build with minification.
+```
+
---
+++ webpack/dev.js
@@ -0,0 +1,59 @@
+
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  watch: true,
+  mode: "development",
+  devtool: 'source-map',
+  entry: [
+    './src/main.js',
+    'webpack/hot/only-dev-server',
+    'webpack-dev-server/client?http://localhost:3000',
+  ],
+  module:{
+    rules: [
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: "style-loader", },
+          { loader: "css-loader", },
+          { loader: "sass-loader", }
+        ]
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            "presets": ['@babel/preset-env']
+          }
+        }]
+      },
+    ],
+  },
+  output: {
+    publicPath: '/',
+    path: path.join(__dirname, '../public'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("development")
+      }
+    }),
+  ],
+  devServer: {
+    contentBase: "./public",  // set "public" path, relative to root
+    noInfo: true,
+    hot: true,
+    inline: true,
+    port: "3000",
+    host: "localhost",
+  }
+};
